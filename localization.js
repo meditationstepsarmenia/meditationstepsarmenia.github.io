62f354715138c8682e8b932a4a15d30c78f314c9
meditationstepsarmenia <ampsarmenia@gmail.com>
2025-03-10 17:22:02 +0400

    localization data

---
+++ localization.js
@@ -0,0 +1,12 @@
+export const tr = {
+    main_header: {
+        en: "Meditation Steps Armenia",
+        ru: "Уроки Медитации Армения",
+        hy: "Մեդիտացիայի Դասեր Հայաստանում"
+    },
+    who_we_are_header: {
+        en: "Who we are?",
+        ru: "Кто мы?",
+        hy: "Ո՞վ ենք մենք"
+    }
+};
