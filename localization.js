f23c390cc7743fe0d894d44c94ce77d324fd8a91
meditationstepsarmenia <ampsarmenia@gmail.com>
2025-03-10 18:26:38 +0400

    localization for "who we are description"

--- localization.js
+++ localization.js
@@ -8,5 +8,16 @@ export const tr = {
         en: "Who we are?",
         ru: "Кто мы?",
         hy: "Ո՞վ ենք մենք"
+    },
+    who_we_are_desc: {
+        en: `We are a community of Rajadhiraja yoga enthusiasts dedicated to spreading the timeless wisdom of yoga. Each week, we hold in-person, donation-based classes, keeping them essentially free to ensure maximum accessibility for everyone. Our sessions include dynamic and static mantra meditations, asanas (yoga postures), yogic dances, and songs, as well as Satsang (spiritual discussions) and lectures.
+We warmly invite you to join us on this unending journey of self-discovery, growth, and global neo-humanistic elevation—together, we’re shaping a more conscious tomorrow. At the heart of our practice is a deep belief in nurturing all three facets of human life: the physical, the mental, and the spiritual.
+Come experience it for yourself! Find our event schedules on the social media channels listed below.`,
+        ru: `Мы — дружное сообщество, посвященное распространению знаний и практик Раджадхираджа-йоги. Каждое воскресенье мы проводим очные занятия, вход на основе пожертвований, а также доступен бесплатно, чтобы обеспечить максимальную доступность для всех. Наши практики включают динамические и статические мантра-медитации, асаны (йогические упражнения), йогические танцы и песни, а также Сатсанг (духовные беседы) и лекции.
+Мы радостно приглашаем вас присоединиться к нам на этом пути непрерывного самопознания, роста и духовного возвышения. Вместе мы создаем осознанное будущее. Мы верим в развитие и гармоничное взаимодействие трех аспектов человеческого существования: физического, ментального и духовного.
+Присоединяйтесь к нам и попробуйте сами! Расписание наших мероприятий можно найти на социальных медиа-платформах, указанных ниже.`,
+        hy: `Մենք ընկերական համայնք ենք, նվիրված Ռաջադհիռաջա յոգայի գիտելիքների և պրակտիկաների տարածմանը։ Ամեն կիրակի մենք անցկացնում ենք առկա դասեր՝ մուտքը նվիրատվությունրով, հասանելի անգամ անվճար, որպեսզի ապահովենք հնարավորինս լայն հասանելիություն բոլորի համար։ Մեր դասերը ներառում են դինամիկ և ստատիկ մանտրա մեդիտացիաներ, ասանաներ (յոգական վարժանքներ), յոգական պարեր և երգեր, ինչպես նաև Սատսանգ (հոգևոր քննարկումներ) և դասախոսություններ։
+Մենք ջերմորեն հրավիրում ենք ձեզ միանալ մեզ այս շարունակական ինքնաբացահայտմանը, աճին և հոգևոր վերելքի ճանապարհին. միասին մենք ձևավորում գիտակից ապագա։ Մենք հավատում և նպաստում ենք մարդկային գոյության երեք ոլորտների զարգացմանը՝ ֆիզիկականը, մտավորն ու հոգևորը։
+Միացեք մեզ՝ փորձեք ինքներդ։ Միջոցառումների ժամանակացույցը կարող եք գտնել ստորև նշված սոցիալական մեդիա-հարթակներում։`,
     }
 };
--- script.js
+++ script.js
@@ -4,12 +4,14 @@ import {tr} from './localization.js';
 const languageItems = document.querySelectorAll(".language-item");
 const main_header = document.querySelector("[class=main-header]");
 const who_we_are_header = document.querySelector("[class=who-we-are-header]");
+const who_we_are_desc = document.querySelector("[class=who-we-are-desc]");
 
 // Set default language (English)
 let currentLang = "en";
 document.querySelector(`[data-lang="en"]`).classList.add("selected");
 main_header.textContent = tr.main_header[currentLang];
 who_we_are_header.textContent = tr.who_we_are_header[currentLang];
+who_we_are_desc.textContent = tr.who_we_are_desc[currentLang];
 
 // Add click event listeners to language items
 languageItems.forEach(item => {
@@ -25,5 +27,6 @@ languageItems.forEach(item => {
         currentLang = item.getAttribute("data-lang");
         main_header.textContent = tr.main_header[currentLang];
         who_we_are_header.textContent = tr.who_we_are_header[currentLang];
+        who_we_are_desc.textContent = tr.who_we_are_desc[currentLang];
     });
 });
