329ae8fd77828be1a23f50443c06ec0798849096
meditationstepsarmenia <ampsarmenia@gmail.com>
2025-09-23 11:55:59 +0400

    major update

--- events.js
+++ events.js
@@ -7,6 +7,10 @@ const location_yfa = {
     desc: "📍Yoga Federation of Armenia",
     link: "https://www.google.com/maps/place/Yoga+Federation+of+Armenia/data=!4m2!3m1!1s0x0:0xe764a5aa6425cbc9?sa=X&ved=1t:2428&ictx=111"
 }
+const location_msa = {
+    desc: "📍Yoga Federation of Armenia",
+    link: "https://www.google.com/maps/place/Meditation+Steps+Armenia/@40.1792191,44.5001857,19z/data=!4m6!3m5!1s0x406abd0042a676c3:0x4736d279f5f19041!8m2!3d40.1792217!4d44.5001217!16s%2Fg%2F11xz5cx01z"
+}
 
 const event_desc_id_suffix = "-event-desc"
 const event_descriptions = {
@@ -20,7 +24,13 @@ const event_descriptions = {
         id: `1${event_desc_id_suffix}`,
         en: "✅Meditation & Yoga With Narek",
         ru: "✅Медитация и Йога с Нареком",
-        am: "Մեդիտացիա և Յոգա Նարեկի հետ",
+        am: "✅Մեդիտացիա և Յոգա Նարեկի հետ",
+    },
+    d3: {
+        id: `1${event_desc_id_suffix}`,
+        en: "🧘‍♂️Yoga/Meditation With Narek",
+        ru: "🧘‍♂️Йога/Медитация с Нареком",
+        am: "Յոգա/Մեդիտացիա Նարեկի հետ 🧘‍♂️",
     }
 }
 
@@ -28,14 +38,14 @@ const events = [
     {
         image: img_2,
         link: fb_event_link,
-        dateTime: `2025-05-04${default_time}`,
-        description: event_descriptions.d2,
-        location: location_yfa,
+        dateTime: `2025-09-28${default_time}`,
+        description: event_descriptions.d3,
+        location: location_msa,
     },{
         image: img_2,
         link: fb_event_link,
-        dateTime: `2025-05-11${default_time}`,
-        description: event_descriptions.d2,
-        location: location_yfa,
+        dateTime: `2025-10-05${default_time}`,
+        description: event_descriptions.d3,
+        location: location_msa,
     },
 ];
--- localization.js
+++ localization.js
@@ -10,34 +10,49 @@ export const tr = {
         am: "Գալիք իրադարձություններ"
     },
     who_we_are_header: {
-        en: "Who we are?",
-        ru: "Кто мы?",
-        am: "Ո՞վ ենք մենք"
+        en: "About us (1 min read)",
+        ru: "О нас (1 минута чтения)",
+        am: "Մեր մասին (կարդալ 1 րոպե)",
     },
     who_we_are_desc: {
-        en: `We are a community of Rajadhiraja yoga enthusiasts dedicated to spreading the timeless wisdom of yoga. Each week, we hold in-person, donation-based classes, keeping them essentially free to ensure maximum accessibility for everyone. Our sessions include dynamic and static mantra meditations, asanas (yoga postures), yogic dances, and songs, as well as Satsang (spiritual discussions) and lectures.
-We warmly invite you to join us on this unending journey of self-discovery, growth, and global neo-humanistic elevation—together, we’re shaping a more conscious tomorrow. At the heart of our practice is a deep belief in nurturing all three facets of human life: the physical, the mental, and the spiritual.
-Come experience it for yourself! Find our event schedules on the social media channels listed below.`,
-        ru: `Мы — дружное сообщество, посвященное распространению знаний и практик Раджадхираджа-йоги. Каждое воскресенье мы проводим очные занятия, вход на основе пожертвований, а также доступен бесплатно, чтобы обеспечить максимальную доступность для всех. Наши практики включают динамические и статические мантра-медитации, асаны (йогические упражнения), йогические танцы и песни, а также Сатсанг (духовные беседы) и лекции.
-Мы радостно приглашаем вас присоединиться к нам на этом пути непрерывного самопознания, роста и духовного возвышения. Вместе мы создаем осознанное будущее. Мы верим в развитие и гармоничное взаимодействие трех аспектов человеческого существования: физического, ментального и духовного.
-Присоединяйтесь к нам и попробуйте сами! Расписание наших мероприятий можно найти на социальных медиа-платформах, указанных ниже.`,
-        am: `Մենք ընկերական համայնք ենք, նվիրված Ռաջադհիռաջա յոգայի գիտելիքների և պրակտիկաների տարածմանը։ Ամեն կիրակի մենք անցկացնում ենք առկա դասեր՝ մուտքը նվիրատվությունրով, հասանելի անգամ անվճար, որպեսզի ապահովենք հնարավորինս լայն հասանելիություն բոլորի համար։ Մեր դասերը ներառում են դինամիկ և ստատիկ մանտրա մեդիտացիաներ, ասանաներ (յոգական վարժանքներ), յոգական պարեր և երգեր, ինչպես նաև Սատսանգ (հոգևոր քննարկումներ) և դասախոսություններ։
-Մենք ջերմորեն հրավիրում ենք ձեզ միանալ մեզ այս շարունակական ինքնաբացահայտմանը, աճին և հոգևոր վերելքի ճանապարհին. միասին մենք ձևավորում գիտակից ապագա։ Մենք հավատում և նպաստում ենք մարդկային գոյության երեք ոլորտների զարգացմանը՝ ֆիզիկականը, մտավորն ու հոգևորը։
-Միացեք մեզ՝ փորձեք ինքներդ։ Միջոցառումների ժամանակացույցը կարող եք գտնել ստորև նշված սոցիալական մեդիա-հարթակներում։`,
+        en: `Hello, dear friend! 🙏
+We are a community that lovingly shares the knowledge and practice of Rajadhiraja Yoga, aiming to foster the harmonious development of individuals on physical, mental, and spiritual levels. 🌸
+📅 Every Sunday, we organize open classes where participation is based on donation, making it accessible to everyone. Our gatherings include:
+✅ Dynamic and static mantra meditations
+✅ Asanas (yogic exercises)
+✅ Yogic dances and songs
+✅ Lectures and spiritual discussions over tea.
+We warmly invite you to join us on this path of self-discovery and spiritual growth. Let's create a conscious future together. You can find the schedule of our events on the social media platforms mentioned above. ⬆️`,
+        ru: `Здравствуйте, дорогой друг! 🙏
+Мы — сообщество, с любовью разделяющее знание и практику Раджадхираджа йоги, целью которого является содействие гармоничному развитию человека на физическом, ментальном и духовном уровнях. 🌸
+📅 Каждое воскресенье мы организуем открытые занятия, участие в которых основано на принципе пожертвования, что делает их доступными для всех. Наши встречи включают:
+✅ Динамические и статические мантра-медитации
+✅ Асаны (йогические упражнения)
+✅ Йогические танцы и песни
+✅ Лекции и духовные беседы за чашкой чая.
+С любовью приглашаем Вас присоединиться к этому пути самопознания и духовного роста. Давайте вместе создавать осознанное будущее. С расписанием наших мероприятий вы можете ознакомиться на указанных выше социальных платформах. ⬆️`,
+        am: `Ողջույն, սիրելի՛ բարեկամ։ 🙏
+Մենք Ռաջադհիռաջա յոգայի գիտելիքն ու պրակտիկան սիրով կիսող համայնք ենք, որի նպատակն է նպաստել մարդու ներդաշնակ զարգացմանը՝ ֆիզիկական, մտավոր և հոգևոր մակարդակներում։ 🌸
+📅 Ամեն կիրակի մենք կազմակերպում ենք բաց դասեր, որոնց մասնակցությունը նվիրատվության սկզբունքով է՝ դարձնելով այն հասանելի բոլորի համար։ Մեր հանդիպումները ներառում են՝
+✅ Դինամիկ և ստատիկ մանտրա-մեդիտացիաներ
+✅ Ասանաներ (յոգական վարժություններ)
+✅ Յոգական պարեր և երգեր
+✅ Դասախոսություններ և հոգևոր զրույցներ թեյի շուրջ:
+Սիրով հրավիրում ենք Ձեզ միանալու ինքնաճանաչման և հոգևոր աճի այս ճանապարհին։ Եկե՛ք միասին կերտենք գիտակից ապագա։ Մեր միջոցառումների ժամանակացույցին կարող եք ծանոթանալ վերը նշված սոցիալական հարթակներում։ ⬆️`,
     },
     social_links: {
         header:  {
-            en: "Social Media Links ✨ Meditation Steps Armenia",
-            ru: "социальные сети ✨ Уроки Медитации Армения",
-            am: "Սոցիալական հարթակներ ✨ Մեդիտացիայի Դասեր Հայաստան",
+            en: "Social Media Links 🔗 ",
+            ru: "Ссылки на Соцсети 🔗 ",
+            am: "Սոցցանցերի Հղումներ 🔗",
         },
         telegram_channel:  {
             en: "Telegram Channel ",
             ru: "Телеграм Канал ",
-            am: "Տելեգրամյան Ալիք ",
+            am: "Տելեգրամ Ալիք ",
         },
         facebook:  {
-            en: "Facebook Page ",
+            en: "Facebook ",
             ru: "Фейсбук ",
             am: "Ֆեյսբուք ",
         },
@@ -47,9 +62,9 @@ Come experience it for yourself! Find our event schedules on the social media ch
             am: "Ինստագրամ ",
         },
         telegram_group:  {
-            en: "Discussion Group Telegram (Mostly Armenian) ",
-            ru: "Группа Дискуссии Телеграм ",
-            am: "Քննարկումների Խումբ Տելեգրամ ",
+            en: "Telegram Group ",
+            ru: "Телеграм Группа",
+            am: "Տելեգրամի Խումբ",
         },
     },
     instructors:  {
@@ -84,7 +99,7 @@ Come experience it for yourself! Find our event schedules on the social media ch
     },
     footer: {
         en: "Kindly, Meditation Steps Armenia team.",
-        ru: "С любовью, команда Уроки Медитации Армения",
-        am: "Հարգանքով, Մեդիտացիայի Դասեր Հայաստանում խումբ",
+        ru: "С любовью, команда Уроки Медитации Армения.",
+        am: "Սիրով, Մեդիտացիայի Դասեր Հայաստանում թիմ:",
     }
 };
